d836170b39f587b9c0244f0a4b6063cd554e0e80
Muhammad Wasil Ali <wasilali4999@gmail.com>
2026-02-13 22:00:03 +0500

    databse setup completed

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv, { config } from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world" });
+});
+app.listen(PORT, () => {
+  console.log(`Listening on port  ${PORT}`);
+});
